2f4d77bf4fbcacbc4c5587fe572688e57ed04ef1
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2021-09-06 10:54:35 +0900

    Added Task4 of 2nd-term-5th-week.

---
+++ Inheritance&Polymorphism/Assets/Endless Slider/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Singleton<T>スクリプト本体(基底クラス)
+/// </summary>
+
+namespace EndlessRunner
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T m_instance;
+
+        protected static T Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = FindObjectOfType<T>();
+                }
+
+                if (m_instance == null)
+                {
+                    string error = string.Format("An instance of {0} is needed in the scene, but there is none.", typeof(T));
+
+                    Debug.LogError(error);
+                }
+
+                return m_instance;
+            }
+        }
+    }
+}
---
+++ Inheritance&Polymorphism/Assets/Endless Slider/Scripts/Singleton/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 基底クラス Singleton<T>から派生したクラス
+/// </summary>
+
+namespace EndlessRunner
+{
+    [AddComponentMenu("CUSTOM / UI Manager")]
+    public class UIManager : Singleton<UIManager>
+    {
+        [SerializeField]
+        private Text m_textCurrentScore;
+        [SerializeField]
+        private Text m_textFinalScore;
+        [SerializeField]
+        private Text m_textBestScore;
+        [SerializeField]
+        private Button m_buttonPlay;
+
+        void Start()
+        {
+            m_buttonPlay.onClick.AddListener(RestartGame);
+        }
+
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                Application.Quit();
+        }
+
+
+        public static void UpdateCurrentScore(int currentScore)
+        {
+            Instance.m_textCurrentScore.text = string.Format("SCORE: {0}", currentScore);
+        }
+
+        public static void UpdateResultScores(int finalScore, int bestScore)
+        {
+            Instance.m_textFinalScore.text = string.Format("FINAL SCORE: {0}", finalScore);
+
+            Instance.m_textBestScore.text = string.Format("BEST SCORE: {0}", bestScore);
+        }
+
+        private void RestartGame()
+        {
+            SoundManager.PlaySoundEffect("ClickButton");
+
+            ScreenFade.Close();
+
+            GlobalVariables.Player.Reload();
+        }
+    }
+}
